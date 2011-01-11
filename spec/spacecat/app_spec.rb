7e5cf1fb90107fa793a305650ff0f11514278e4b
Kristján Pétursson <kristjan@causes.com>
2011-01-10 17:49:12 -0800

    Fix spec to allow for negative spacecat fitness

--- spec/spacecat/app_spec.rb
+++ spec/spacecat/app_spec.rb
@@ -16,7 +16,7 @@ describe Spacecat::App do
   it "scores your cat for you" do
     get '/andromeda', {:weight => 2, :limbs => 4, :color => '000002'}
     last_response.should be_successful
-    last_response.body.should =~ /^\d+$/
+    last_response.body.to_i.to_s.should == last_response.body
   end
 
   it "gives you a reasonable error for an unreasonable cat" do
